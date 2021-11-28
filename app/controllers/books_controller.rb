6e347febafb891dd63a5156c4e2f7d2f5202237d
RouxRhett <nsch.4871@gmail.com>
2021-11-28 18:39:28 +0900

    [add]bookモデルの編集、削除が行える様にした。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,35 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = 'You have created book successfully.'
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user_id != current_user.id
+      redirect_to books_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = 'You have updated book successfully.'
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
 
   def index
@@ -10,14 +37,18 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
 
-  def post_image_params
+  def book_params
     params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to:'homes#top'
   get 'home/about', to: 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :edit, :update,:index, :show, :destroy]
 
 
 end
