44eeb22623efa4684eea34fc51a25b27bd5f9ded
RouxRhett <nsch.4871@gmail.com>
2021-11-28 23:48:54 +0900

    [add]userモデルの編集機能を追加した。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,33 @@
 class UsersController < ApplicationController
   def index
-
+    @users = User.page(params[:page]).reverse_order
   end
 
   def show
-    @u
+    @user = User.find(params[:id])
+    @books = @user.books.page(params[:page]).reverse_order
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user.id != current_user.id
+      redirect_to user_path(current_user.id)
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = 'You have updated user successfully.'
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books, only: [:new, :create, :edit, :update,:index, :show, :destroy]
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :edit, :update]
 
 end
